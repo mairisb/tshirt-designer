35051f613cc275fbb58bf6529faac5b840924db6
mairisb <berzinsmairis@gmail.com>
2023-09-04 20:39:34 +0300

    update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,34 +3,40 @@ import { ref } from "vue";
 import Canvas from "../components/Canvas.vue";
 import LayoutPlacementForm from "../components/LayoutPlacementForm.vue";
 
-const tab = ref<string | null>(null);
+const currentTab = ref<string | null>(null);
 </script>
 
 <template>
   <h1 class="text-h4 mb-4">Design your T-shirt</h1>
+
   <v-card>
     <v-card-text><Canvas></Canvas></v-card-text>
 
-    <v-tabs v-model="tab" fixed-tabs center-active>
-      <v-tab value="print-area">Print area</v-tab>
-      <v-tab value="design">Design</v-tab>
+    <v-tabs v-model="currentTab" fixed-tabs center-active>
+      <v-tab value="print-areas">Placement areas</v-tab>
+      <v-tab value="designs">Designs</v-tab>
     </v-tabs>
 
     <v-card-text>
-      <v-window v-model="tab" class="pt-0">
-        <v-window-item value="print-area">
+      <v-window v-model="currentTab" class="pt-0">
+        <v-window-item value="print-areas">
           <h3 class="text-h6">Front</h3>
           <LayoutPlacementForm position="front" />
+
           <h3 class="text-h6">Back</h3>
           <LayoutPlacementForm position="back" />
+
           <h3 class="text-h6">Left sleeve</h3>
           <LayoutPlacementForm position="leftSleeve" />
+
           <h3 class="text-h6">Right sleeve</h3>
           <LayoutPlacementForm position="rightSleeve" />
         </v-window-item>
-        <v-window-item value="design">
+
+        <v-window-item value="designs">
           <h3 class="text-h6">Rectangle 1</h3>
           <LayoutPlacementForm layer="rectangle1" position="front" />
+
           <h3 class="text-h6">Rectangle 2</h3>
           <LayoutPlacementForm layer="rectangle2" position="front" />
         </v-window-item>
