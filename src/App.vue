e4a2cc9c6dc99252a51cc5e5b388215e87583bf1
mairisb <berzinsmairis@gmail.com>
2023-09-04 11:24:39 +0300

    create basic app layout

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
+<script setup lang="ts">
+import { RouterView } from "vue-router";
+</script>
+
 <template>
   <v-app>
     <v-main>
-      <HelloWorld />
+      <v-app-bar color="primary" density="compact">
+        <v-app-bar-title>T-shirt Designer</v-app-bar-title>
+      </v-app-bar>
+      <v-container>
+        <RouterView />
+      </v-container>
     </v-main>
   </v-app>
 </template>
-
-<script setup lang="ts">
-  import HelloWorld from '@/components/HelloWorld.vue'
-</script>
